68cd54ced2317d8fb78daaf27798dcb69d744324
demola <demolasobaki@gmail.com>
2019-08-28 10:00:56 +0100

    Selected html data attributes from html page via document model

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButton = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deletesButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
