8af54e93854876fdabee18455e5b10a0cec7f940
demola <demolasobaki@gmail.com>
2019-08-28 11:42:53 +0100

    Error Fixes

--- script.js
+++ script.js
@@ -54,9 +54,22 @@ class Calculator {
   }
 
   getDisplay(number) {
-    const floatNumber = parseFloat(number);
-    if (isNaN(floatNumber)) return "";
-    return floatNumber.toLocaleString("en");
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
   }
 
   updateDisplay() {
@@ -64,7 +77,11 @@ class Calculator {
       this.currentOperand
     );
     if (this.operation != null) {
-      this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+      this.previousOperandTextElement.innerText = `${this.getDisplay(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
