2d63de73217d9fdaa7ba4121135cbc2deeeacc8a
demola <demolasobaki@gmail.com>
2019-08-28 11:22:23 +0100

    Added functionality to add, clear and delete numbers

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ class Calculator {
     this.previousOperand = "";
     this.operation = undefined;
   }
-  delete() {}
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
   appendNumber(number) {
     if (number === "." && this.currentOperand.includes(".")) return;
     this.currentOperand = this.currentOperand.toString() + number.toString();
@@ -25,7 +27,31 @@ class Calculator {
     this.previousOperand = this.currentOperand;
     this.currentOperand = "";
   }
-  compute() {}
+  compute() {
+    let computation;
+    const previousValue = parseFloat(this.previousOperand);
+    const currentValue = parseFloat(this.currentOperand);
+    if (isNaN(previousValue) || isNaN(currentValue)) return;
+    switch (this.operation) {
+      case "+":
+        computation = previousValue + currentValue;
+        break;
+      case "-":
+        computation = previousValue - currentValue;
+        break;
+      case "*":
+        computation = previousValue * currentValue;
+        break;
+      case "÷":
+        computation = previousValue / currentValue;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
@@ -36,7 +62,7 @@ class Calculator {
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
-const deletesButton = document.querySelector("[data-delete]");
+const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
 const previousOperandTextElement = document.querySelector(
   "[data-previous-operand]"
@@ -45,11 +71,13 @@ const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
 
+// created instance of calculator class
 const calculator = new Calculator(
   previousOperandTextElement,
   currentOperandTextElement
 );
 
+//select numbers and update display
 numberButtons.forEach(button =>
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
@@ -57,9 +85,25 @@ numberButtons.forEach(button =>
   })
 );
 
+//select operation (addition, subtraction, multiplication & division) and update display
 operationButtons.forEach(button =>
   button.addEventListener("click", () => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   })
 );
+
+equalsButton.addEventListener("click", button => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", button => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", button => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
